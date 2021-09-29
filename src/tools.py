54922015b404df49e85afcb907079e5adba958a3
JulianTrufero <jgtrufero@gmail.com>
2021-09-29 23:53:13 +0200

    update

---
+++ src/tools.py
@@ -0,0 +1,11 @@
+def vehpower_gr(x):
+    """
+    ARG: A number
+    Returns: If is different from 12, 13, 14 or 15, returns the number, 
+    otherwise, returns 12
+    """
+    if x in [12, 13, 14, 15]:
+        x_ = 12
+        return x_
+    else:
+        return x
