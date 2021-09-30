50dc2a814b11a7ac800bc0eb848c08e136ebb3bd
JulianTrufero <jgtrufero@gmail.com>
2021-09-30 11:52:09 +0200

    update

--- src/tools.py
+++ src/tools.py
@@ -9,3 +9,49 @@ def vehpower_gr(x):
         return x_
     else:
         return x
+
+def vehage_gr(x):
+    """
+    ARG: A number
+    Returns: If x belongs to (0,1) -> 1
+             If x belongs to (1,10) -> 2
+             If x belongs to (10,100) -> 3
+    """
+    if x in range(2):
+        x_ = 1
+        return x_
+    elif x in range(1,11):
+        x_ = 2
+        return x_
+    else:
+        return 3
+
+def drivage_gr(x):
+    """
+    ARG: A number
+    Returns: Assings a class (from 1 to 8) if x belongs to the specified ranges
+    """
+    if x in range(18,21):
+        x_ = 1
+        return x_
+    elif x in range(21,26):
+        x_ = 2
+        return x_
+    elif x in range(26,31):
+        x_ = 3
+        return x_
+    elif x in range(31,41):
+        x_ = 4
+        return x_
+    elif x in range(41,51):
+        x_ = 5
+        return x_
+    elif x in range(51,61):
+        x_ = 6
+        return x_
+    elif x in range(61,71):
+        x_ = 7
+        return x_
+
+    else:
+        return 8
